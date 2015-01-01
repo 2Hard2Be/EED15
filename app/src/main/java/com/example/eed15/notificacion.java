8d1288c854dbb63a94960cd4e7307397a158d27d
2Hard2Be <luisrivas.power@gmail.com>
2014-12-31 21:47:13 -0600

    Se activa el temporizador y la notificacion

--- app/src/main/java/com/example/eed15/notificacion.java
+++ app/src/main/java/com/example/eed15/notificacion.java
@@ -1,9 +1,18 @@
 package com.example.eed15;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Created by Peto-1 on 12/30/2014.
@@ -11,7 +20,8 @@ import android.support.v4.app.NotificationCompat;
 public class notificacion extends Activity {
 
 
-public void creadorNotificacion() {
+    public void sapito() {
+
 
 //    Creando Notification builder
         NotificationCompat.Builder constructorNotificacion = new NotificationCompat.Builder(this);
@@ -28,29 +38,55 @@ public void creadorNotificacion() {
 //crando pendint intent para llamar al inentalMain, ATENTO QUE NO SE SABE SI FLAG_UPDATE_CURRENT
 //    ES REALMENTE NECESARIO?????
 
-    PendingIntent napoLeon = PendingIntent.getActivity(this,0,intentalMain,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent napoLeon = PendingIntent.getActivity(this, 0, intentalMain, PendingIntent.FLAG_UPDATE_CURRENT);
 
 //    colocando pendint intent napoLeon al Notificationcompat.Builder llamado constructorNotificacion, creado
 //    anteiormente
-    constructorNotificacion.setContentIntent(napoLeon);
+        constructorNotificacion.setContentIntent(napoLeon);
 
 //    agrengando ticker ESTE SOLO ES PLANTE LO PODEMOS QUITAR pero diversion va primero
 
-    constructorNotificacion.setTicker(getResources().getString(R.string.ticker));
+        constructorNotificacion.setTicker(getResources().getString(R.string.ticker));
 
 //    Creando la imagen de la barra de tareas
 
-    constructorNotificacion.setSmallIcon(R.drawable.noti_ico);
+        constructorNotificacion.setSmallIcon(R.drawable.noti_ico);
 
 //    Cancelando la notificacion al darle clic
 
-    constructorNotificacion.setAutoCancel(true);
+        constructorNotificacion.setAutoCancel(true);
 
 //    ATENCION AQUI SEGUI CREANDO LA ANIMACION DE ABRIR EL EXAMINANDO SEGUN EL EJEMPLO DE CUSTOM
-//    NOTIFICATION, ANTES PROBA LA APP CREANDO EL MAIN
+//    NOTIFICATION, ANTES PROBA LA APP CREANDO EL MAIN, DE AQUI EN ADELANTE A LA PASARRAYA SOLO
+//        PARA PROBAR
+
+        Notification notificarle = constructorNotificacion.build();
+
+        RemoteViews vistasdeContenido = new RemoteViews(getPackageName(), R.layout.notif);
+        final String time = DateFormat.getTimeInstance().format(new Date()).toString();
+        final String text = getResources().getString(R.string.colapsado, time);
+        vistasdeContenido.setTextViewText(R.id.textView, text);
+//        alerta con este contentview tal vez se refuere a vustas de contenido
+        notificarle.contentView = vistasdeContenido;
+
+        if (Build.VERSION.SDK_INT >= 16) {
+            // Inflate and set the layout for the expanded notification view
+            RemoteViews expandedView =
+                    new RemoteViews(getPackageName(), R.layout.notificacion_expandida);
+            notificarle.bigContentView = expandedView;
+            NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            nm.notify(0, notificarle);
+
+        }
 
 
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sapito();
+        Toast.makeText(notificacion.this, "10 segundos", Toast.LENGTH_LONG).show();
+    }
 
 }
--- app/src/main/java/com/example/eed15/repeatingAlarm.java
+++ app/src/main/java/com/example/eed15/repeatingAlarm.java
@@ -2,9 +2,12 @@ package com.example.eed15;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.Fragment;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.MenuItem;
 import android.widget.Toast;
 import java.util.Calendar;
 
@@ -24,34 +27,43 @@ public class repeatingAlarm extends Activity {
     public void onCreate (Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
 
+
+
 //    Falta crear la clase notificacion (tendria que ser el intent que llame la notificacion de la barra)
-    Intent intentNotification = new Intent(repeatingAlarm.this, notificacion.class);
+    Intent intentNotification = new Intent(repeatingAlarm.this,notificacion.class);
 
 
 //    Falta usar el pending intent, contexto repeatingAlarm y llamara al intentNotification que debera
 //    invocar la notificacion en la barra de tareas
-    PendingIntent pendingIntentNoti = PendingIntent.getService(repeatingAlarm.this,0,intentNotification,0);
+    PendingIntent pendingIntentNoti = PendingIntent.getActivity(repeatingAlarm.this,0,intentNotification,0);
 
 //    Aqui crea el alarmManager, guiate segun la pagina web android-er y los ejemlos
 //Inicializando objeto del tipo alarma...beeep....beeep
 
-    AlarmManager laAlarma = (AlarmManager) getSystemService(ALARM_SERVICE);
+   int alarmType = AlarmManager.ELAPSED_REALTIME;
+    final int FIFTEEN_SEC_MILLIS = 10000;
+    AlarmManager alarmManager = (AlarmManager)
+
+//            ATENCION AQUI MODIFIQUE Y QUITE EL REPEATING ALARM COMO CONTEXTO
+            getSystemService(repeatingAlarm.ALARM_SERVICE);
+    alarmManager.setRepeating(alarmType, SystemClock.elapsedRealtime() + FIFTEEN_SEC_MILLIS,
+            FIFTEEN_SEC_MILLIS, pendingIntentNoti);
+
+    Toast.makeText(repeatingAlarm.this,"Alarma 10 segundos",Toast.LENGTH_SHORT).show();
 
 //    Creando un objeto de la clase Calendar para determinar la fecha actual en la app, AHUUUU!!!
 
-    Calendar calendario = Calendar.getInstance();
-    calendario.setTimeInMillis(System.currentTimeMillis());
+
 
 //    PARA FINES DE PRUEBA SE AJUSTO EL CALENDARIO A SUMAR 10 SEGUNDOS A LA HORA ACTUAL
 //    EN EL PROGRAMA DEBERAS ECONTRAR LA FORMA DE QUE LLEGUE A LA MISMA HORA DEL DIA PARA EL CASO
 //    6:00 AM O 12:00 AM
 
-    calendario.add(Calendar.SECOND,10);
-    laAlarma.set(AlarmManager.RTC_WAKEUP, calendario.getTimeInMillis(), pendingIntentNoti );
+
 
 //    Este Toast solo es para indicar que la alarma ya esta hecha, para el mero programa
 //    tu decides si se queda o no
-    Toast.makeText(repeatingAlarm.this, "Lee la biblia diariamente", Toast.LENGTH_LONG).show();
+
 
 
 //APARENTEMENTE YA ESTA HECHA LA ALARMA
